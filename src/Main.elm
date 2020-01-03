b1fec89a605fd24a0cc895b911e874457c10fb2a
kosappi <kosappi@icloud.com>
2020-01-03 21:56:44 +0900

    3-1

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,27 @@
-import Html exposing (Html, a, text)
+module Main exposing (main)
+
+import Html exposing (Html, a, div, h1, li, text, ul)
 import Html.Attributes exposing (href)
+
 
 main : Html msg
 main =
-    a [ href "https://elm-lang.org" ] [ text "Elm" ]
+    div [] [ header, content ]
+
+
+header : Html msg
+header =
+    h1 [] [ text "Useful Links" ]
+
+
+content : Html msg
+content =
+    ul []
+        [ linkItem "https://elm-lang.org" "Homepage"
+        , linkItem "https://package.elm-lang.org" "Packages"
+        , linkItem "https://ellie-app.com" "Playground"
+        ]
+
+linkItem : String -> String -> Html msg
+linkItem url text_ =
+    li [] [ a [ href url ] [ text text_ ] ]
