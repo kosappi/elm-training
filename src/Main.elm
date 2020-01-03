affea67999d3d1defa031d4652a4d1a9e45edce3
kosappi <kosappi@icloud.com>
2020-01-03 21:01:16 +0900

    3-0

--- src/Main.elm
+++ src/Main.elm
@@ -1,1 +1,6 @@
-module Main exposing (..)
+import Html exposing (Html, a, text)
+import Html.Attributes exposing (href)
+
+main : Html msg
+main =
+    a [ href "https://elm-lang.org" ] [ text "Elm" ]
