aa82bb585048fd3f57f4ede0e6602ae80d7d5ec0
kosappi <kosappi@icloud.com>
2020-01-13 15:05:53 +0900

    use batch

--- src/Main.elm
+++ src/Main.elm
@@ -1,68 +1,83 @@
+module Main exposing (main)
+
 import Browser
 import Html exposing (..)
-import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Http
+
 
 main : Program () Model Msg
 main =
-    Browser.sandbox
+    Browser.element
         { init = init
-        , update = update
         , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
         }
+
+
 
 -- MODEL
 
+
 type alias Model =
-    { input : String
-    , memos : List String
+    { resultCore : String
+    , resultSvg : String
     }
 
-init : Model
-init =
-    { input = ""
-    , memos = []
-    }
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { resultCore = "", resultSvg = "" }
+    , Cmd.none
+    )
+
+
 
 -- UPDATE
 
-type Msg
-    = Input String
-    | Submit
-    | Delete
 
-update : Msg -> Model -> Model
+type Msg
+    = Click
+    | GotCoreModule (Result Http.Error String)
+    | GotSvgModule (Result Http.Error String)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Input input ->
-            -- 入力文字列を更新する
-            { model | input = input }
+        Click ->
+            ( model
+            , Cmd.batch
+                [ Http.get
+                    { url = "https://api.github.com/repos/elm/core"
+                    , expect = Http.expectString GotCoreModule
+                    }
+                , Http.get
+                    { url = "https://api.github.com/repos/elm/svg"
+                    , expect = Http.expectString GotSvgModule
+                    }
+                ]
+            )
 
-        Submit ->
-            { model
-                | input = ""
-                , memos = model.input :: model.memos
-            }
+        GotCoreModule (Ok repo) ->
+            ( { model | resultCore = repo }, Cmd.none )
 
-        Delete ->
-            { model
-                | memos = model.memos |> List.drop 1  }
+        GotCoreModule (Err error) ->
+            ( { model | resultCore = Debug.toString error }, Cmd.none )
+
+        GotSvgModule (Ok repo) ->
+            ( { model | resultSvg = repo }, Cmd.none )
+
+        GotSvgModule (Err error) ->
+            ( { model | resultSvg = Debug.toString error }, Cmd.none )
 
 -- VIEW
 
 view : Model -> Html Msg
 view model =
     div []
-        [ Html.form [ onSubmit Submit ]
-            [ input [ value model.input, onInput (\s -> Input s) ] []
-            , button
-                [ disabled (String.length model.input < 1) ]
-                [ text "Submit" ]
-            ]
-        , Html.form [ onSubmit Delete ] [ button [ disabled (List.length model.memos < 1) ] [ text "Delete" ] ]
-        , ul [] (List.map viewMemo model.memos)
+        [ button [ onClick Click ] [ text "Get Repository Info" ]
+        , p [] [ text model.resultCore ]
+        , p [] [ text model.resultSvg ]
         ]
-
-viewMemo : String -> Html Msg
-viewMemo memo =
-    li [] [ text memo ]
